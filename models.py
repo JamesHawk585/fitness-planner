f4c57a3e3a13f167874a0489a359e53606d2de39
James Hawk <jph94880@gmail.com>
2023-08-20 20:07:09 -0500

    Adds Workouts and Exercises classes in models.py

--- models.py
+++ models.py
@@ -1,4 +1,23 @@
+from sqlalchemy import Column, Integer, String 
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+class Workouts(Base):
+    __tablename__ = "workouts"
+    id = Column(Integer, primary_key=True)
+    workout_name = Column(String)
+    wortout_description = Column(String)
+
+class Exercises(Base):
+    __tablename__ = "exercises"
+    id = Column(Integer, primary_key=True)
+    exercise_description= Column(String)
+    category = Column(String)
+    weight = Column(Integer)
+    units = Column(String)
+    reps = Column(Integer)
+    sets = Column(Integer)
+
+    
 
