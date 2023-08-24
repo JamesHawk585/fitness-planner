67cbbf8331ae331d86ee06ba49dbf49d48f94c49
James Hawk <jph94880@gmail.com>
2023-08-23 20:01:22 -0500

    Moves handle_slection() and handle_user_input() inside the Cli class.

--- lib/cli.py
+++ lib/cli.py
@@ -11,32 +11,22 @@ class Cli():
         options = ["Create a new workout", "View existing workouts", "Add exercise to workout", "Delete exercise from workout"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
+        input = menu_entry_index
         print(f"You have selected {options[menu_entry_index]}!")
-    # def handle_user_input(input):
-    # # import ipdb; ipdb.set_trace()
-    #     is_number = input.isdigit()
-    #     if is_number:
-    #         selection = int(input)
-    #         if 1 <= selection <=5:
-    #             handle_selection(selection)
-    #     else: 
-    #         print(red("Please enter a valid number (1-5)."))
-    start()
-    # handle_user_input()
-
-
-def handle_selection():
-    pass
-
-def handle_user_input(input):
+    def handle_user_input(input):
     # import ipdb; ipdb.set_trace()
-    is_number = input.isdigit()
-    if is_number:
-        selection = int(input)
-        if 1 <= selection <=5:
-            handle_selection(selection)
-    else: 
-        print(red("Please enter a valid number (1-5)."))
+        is_number = input.isdigit()
+        if is_number:
+            selection = int(input)
+            if 1 <= selection <=5:
+                handle_selection(selection)
+        else: 
+            print(red("Please enter a valid number (1-5)."))
+    def handle_selection():
+        pass
+    start()
+    handle_user_input()
+    handle_selection()
 
     def create_workout():
         pass
