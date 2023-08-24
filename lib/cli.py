8952cfbe975b4985a8adfb63121b07ebee31ae16
James Hawk <jph94880@gmail.com>
2023-08-23 19:36:43 -0500

    Adds Cli class to cli.py. Refactors start() function to use simple_term_menu.

--- lib/cli.py
+++ lib/cli.py
@@ -1,44 +1,45 @@
 # from ipdb import set_trace
+from prettycli import red, bold
 import os
+from simple_term_menu import TerminalMenu
 
-def start():
-    print("Welcome to the Fitness Planner! 💪")
-    print('1. Create a new workout')
-    print('2. View existing workouts')
-    print('3. Add exercise to workout')
-    print('4. Delete exercise from workout')
-    print('5. Exit')
-    user_input = input('What would you like to do?')
-    handle_user_input(user_input)
+# Create simple-term-menu
+
+
+class Cli():
+    def start():
+        print(bold("Welcome to the Fitness Planner! 💪"))
+        options = ["Create a new workout", "View existing workouts", "Add exercise to workout", "Delete exercise from workout"]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        print(f"You have selected {options[menu_entry_index]}!")
+    start()
+
+# def start():
+#     print(bold("Welcome to the Fitness Planner! 💪"))
+#     print('1. Create a new workout')
+#     print('2. View existing workouts')
+#     print('3. Add exercise to workout')
+#     print('4. Delete exercise from workout')
+#     print('5. Exit')
+#     user_input = input('What would you like to do?')
+#     handle_user_input(user_input)
+
+
+def handle_selection():
+    pass
 
 def handle_user_input(input):
     # import ipdb; ipdb.set_trace()
-    if input == '1':
-        print('Creating workout...')
-        # Returns the next workout in a rotation of push_day, pull_day, and abs_and_legs_day.
-    elif input == '2':
-        print('Showing existing workouts...')
-        #  Return the exercises for each of the three workouts.
-    elif input == '3':
-        print('Adding exercise to workout...')
-        # Add new exercise to a specified workout. 
-            #  User will have to enter a valid value for each of the columns in the exercise table and append that exercise to an existing workout. 
-    elif input == '4':
-        print('Deleting exercise from workout...')
-        # Delete a specified instance of the Exercise class by refrencing exercise_name.
-    elif input == '5': 
-        print('Quitting app..')
-        # Quit the application 
+    is_number = input.isdigit()
+    if is_number:
+        selection = int(input)
+        if 1 <= selection <=5:
+            handle_selection(selection)
     else: 
-        os.system('clear')
-        print('Please enetr a valid number (1-5).')
-        start()
+        print(red("Please enter a valid number (1-5)."))
 
 
-
-start()
-
-    
 
 
 
